926666aadc99307d0e09e2bb6f475f44de8992f1
leeyoonseo <okayoon.lee@gmail.com>
2020-07-23 23:36:00 +0900

    생각정리 주석추가

--- assest/js/index.js
+++ assest/js/index.js
@@ -1,10 +1,15 @@
 'use strict';
 
+// 클래스로 item들 빼자
+// sorting --> check한거 안 한거
+// 마지막에는 배열 합쳐서 넘기기
+
 var TripCheckList = (function(){
     var CHECK_LIST_DATA = 'CHECK_LIST_DATA';
     
     // localStorage.removeItem(CHECK_LIST_DATA);
-    var data = localStorage.getItem(CHECK_LIST_DATA) || '[{"id":"C01","checked":"true","name":"여벌 옷"},{"id":"C02","checked":"false","name":"실내 복"},{"id":"C03","checked":"false","name":"수영복"},{"id":"C04","checked":"false","name":"모자"},{"id":"C05","checked":"false","name":"슬리퍼"},{"id":"C06","checked":"false","name":"샴푸&린스"},{"id":"C07","checked":"false","name":"바디워시"},{"id":"C08","checked":"false","name":"클렌징"},{"id":"C09","checked":"false","name":"스킨케어"},{"id":"C010","checked":"false","name":"마스크팩"},{"id":"C011","checked":"false","name":"드라이기&고데기"},{"id":"C012","checked":"false","name":"수건"},{"id":"C013","checked":"false","name":"화장품"},{"id":"C014","checked":"false","name":"여권"},{"id":"C015","checked":"false","name":"휴지&물티슈"},{"id":"C016","checked":"false","name":"카메라"},{"id":"C017","checked":"false","name":"충전기"},{"id":"C018","checked":"false","name":"노트북"},{"id":"C019","checked":"false","name":"선글라스"},{"id":"C020","checked":"false","name":"선크림"},{"id":"C021","checked":"false","name":"우산, 비옷"},{"id":"C022","checked":"false","name":"이어폰"},{"id":"C023","checked":"false","name":"비상약"},{"id":"C024","checked":"false","name":"서브가방"},{"id":"C025","checked":"false","name":"지퍼백"},{"id":"C026","checked":"false","name":"속옷"},{"id":"C027","checked":"false","name":"면도기"},{"id":"C028","checked":"false","name":"가이드북"},{"id":"C029","checked":"false","name":"돼지코"}]';
+    console.log(localStorage.getItem(CHECK_LIST_DATA))
+    var data = localStorage.getItem(CHECK_LIST_DATA) || '[{"id":"C01","checked":"false","name":"여벌 옷"},{"id":"C02","checked":"false","name":"실내 복"},{"id":"C03","checked":"false","name":"수영복"},{"id":"C04","checked":"false","name":"모자"},{"id":"C05","checked":"false","name":"슬리퍼"},{"id":"C06","checked":"false","name":"샴푸&린스"},{"id":"C07","checked":"false","name":"바디워시"},{"id":"C08","checked":"false","name":"클렌징"},{"id":"C09","checked":"false","name":"스킨케어"},{"id":"C010","checked":"false","name":"마스크팩"},{"id":"C011","checked":"false","name":"드라이기&고데기"},{"id":"C012","checked":"false","name":"수건"},{"id":"C013","checked":"false","name":"화장품"},{"id":"C014","checked":"false","name":"여권"},{"id":"C015","checked":"false","name":"휴지&물티슈"},{"id":"C016","checked":"false","name":"카메라"},{"id":"C017","checked":"false","name":"충전기"},{"id":"C018","checked":"false","name":"노트북"},{"id":"C019","checked":"false","name":"선글라스"},{"id":"C020","checked":"false","name":"선크림"},{"id":"C021","checked":"false","name":"우산, 비옷"},{"id":"C022","checked":"false","name":"이어폰"},{"id":"C023","checked":"false","name":"비상약"},{"id":"C024","checked":"false","name":"서브가방"},{"id":"C025","checked":"false","name":"지퍼백"},{"id":"C026","checked":"false","name":"속옷"},{"id":"C027","checked":"false","name":"면도기"},{"id":"C028","checked":"false","name":"가이드북"},{"id":"C029","checked":"false","name":"돼지코"}]';
     
     var myListArr = [];
     var listArr = [];
@@ -76,19 +81,21 @@ var TripCheckList = (function(){
     };
 
     saveBtn.addEventListener('click', function(){
-        myListData.sort(function(a, b){
+        myListArr.sort(function(a, b){
             return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
         });
 
-        for(var i = 0; i < myListData.length; i++){
-            for(var j = 0; j < STORAGE_DATA.length; j++){
-                if(STORAGE_DATA[j].id === myListData[i].id){
-                    STORAGE_DATA[j].checked = "true";
+        for(var i = 0; i < myListArr.length; i++){
+            for(var j = 0; j < data.length; j++){
+                if(data[j].id === myListArr[i].id){
+                    data[j].checked = "true";
+                }else{
+                    data[j].checked = "false";
                 }
             }
         }
-
-        localStorage.setItem(CHECK_LIST_DATA, JSON.stringify(STORAGE_DATA));
+        console.log(data)
+        localStorage.setItem(CHECK_LIST_DATA, JSON.stringify(data));
     });
 
     init();
