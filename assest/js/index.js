400a1d52fcbb951410718578355a1e6ed431207e
leeyoonseo <okayoon.lee@gmail.com>
2020-07-25 22:56:59 +0900

    includes polyfill추가, 검색기능추가, 검색버튼 삭제

--- assest/js/index.js
+++ assest/js/index.js
@@ -1,10 +1,15 @@
 (function(){
     'use strict';
 
-    var CHECK_LIST_DATA = 'CHECK_LIST_DATA';
+    var TRIP_CHECK_LIST = {
+        CHECK_LIST_DATA : 'CHECK_LIST_DATA',
+        NOTI_TEXT_DEFAULT : '완료되었습니다.',
+        NOTI_TEXT_SAVE : '저장되었습니다.',
+        NOTI_TEXT_SEARCH_EMPTY : '검색어를 입력해주세요.',
+    }
 
     // localStorage.removeItem(CHECK_LIST_DATA);
-    var data = localStorage.getItem(CHECK_LIST_DATA) || '[{"id":"C01","checked":"false","name":"여벌 옷"},{"id":"C02","checked":"false","name":"실내 복"},{"id":"C03","checked":"false","name":"수영복"},{"id":"C04","checked":"false","name":"모자"},{"id":"C05","checked":"false","name":"슬리퍼"},{"id":"C06","checked":"false","name":"샴푸&린스"},{"id":"C07","checked":"false","name":"바디워시"},{"id":"C08","checked":"false","name":"클렌징"},{"id":"C09","checked":"false","name":"스킨케어"},{"id":"C010","checked":"false","name":"마스크팩"},{"id":"C011","checked":"false","name":"드라이기&고데기"},{"id":"C012","checked":"false","name":"수건"},{"id":"C013","checked":"false","name":"화장품"},{"id":"C014","checked":"false","name":"여권"},{"id":"C015","checked":"false","name":"휴지&물티슈"},{"id":"C016","checked":"false","name":"카메라"},{"id":"C017","checked":"false","name":"충전기"},{"id":"C018","checked":"false","name":"노트북"},{"id":"C019","checked":"false","name":"선글라스"},{"id":"C020","checked":"false","name":"선크림"},{"id":"C021","checked":"false","name":"우산, 비옷"},{"id":"C022","checked":"false","name":"이어폰"},{"id":"C023","checked":"false","name":"비상약"},{"id":"C024","checked":"false","name":"서브가방"},{"id":"C025","checked":"false","name":"지퍼백"},{"id":"C026","checked":"false","name":"속옷"},{"id":"C027","checked":"false","name":"면도기"},{"id":"C028","checked":"false","name":"가이드북"},{"id":"C029","checked":"false","name":"돼지코"}]';
+    var data = localStorage.getItem(TRIP_CHECK_LIST.CHECK_LIST_DATA) || '[{"id":"C01","checked":"false","name":"여벌 옷"},{"id":"C02","checked":"false","name":"실내 복"},{"id":"C03","checked":"false","name":"수영복"},{"id":"C04","checked":"false","name":"모자"},{"id":"C05","checked":"false","name":"슬리퍼"},{"id":"C06","checked":"false","name":"샴푸&린스"},{"id":"C07","checked":"false","name":"바디워시"},{"id":"C08","checked":"false","name":"클렌징"},{"id":"C09","checked":"false","name":"스킨케어"},{"id":"C010","checked":"false","name":"마스크팩"},{"id":"C011","checked":"false","name":"드라이기&고데기"},{"id":"C012","checked":"false","name":"수건"},{"id":"C013","checked":"false","name":"화장품"},{"id":"C014","checked":"false","name":"여권"},{"id":"C015","checked":"false","name":"휴지&물티슈"},{"id":"C016","checked":"false","name":"카메라"},{"id":"C017","checked":"false","name":"충전기"},{"id":"C018","checked":"false","name":"노트북"},{"id":"C019","checked":"false","name":"선글라스"},{"id":"C020","checked":"false","name":"선크림"},{"id":"C021","checked":"false","name":"우산, 비옷"},{"id":"C022","checked":"false","name":"이어폰"},{"id":"C023","checked":"false","name":"비상약"},{"id":"C024","checked":"false","name":"서브가방"},{"id":"C025","checked":"false","name":"지퍼백"},{"id":"C026","checked":"false","name":"속옷"},{"id":"C027","checked":"false","name":"면도기"},{"id":"C028","checked":"false","name":"가이드북"},{"id":"C029","checked":"false","name":"돼지코"}]';
 
     var myListArr = [];
     var listArr = [];
@@ -12,7 +17,23 @@
     var myListEl = document.querySelector('.app__mylist .list__wrap');
     var listEl = document.querySelector('.app__list .list__wrap');
     var searchInput = document.querySelector('#searchInput');
+    // var searchButton = document.querySelector('#searchButton');
     var saveBtnEl = document.querySelector('#saveButton');
+
+    // polyfill
+    if (!String.prototype.includes) {
+        String.prototype.includes = function(search, start) {
+            if (typeof start !== 'number') {
+                start = 0;
+            }
+
+            if (start + search.length > this.length) {
+                return false;
+            } else {
+                return this.indexOf(search, start) !== -1;
+            }
+        };
+    };
 
     var CheckItem = function(mode, data){
         this.mode = mode;
@@ -67,7 +88,8 @@
     var Notification = function(opts){
         this.options = Object.assign({},{
             timer : 1,
-            text : '저장되었습니다.',
+            text : TRIP_CHECK_LIST.NOTI_TEXT_DEFAULT,
+            imageUrl : '',
             target : 'section' // id, class, tag 
         }, opts);
 
@@ -82,17 +104,20 @@
 
         wrap.classList.add('app__noti');
         inner.classList.add('inner');
-        inner.innerText = this.options.text;
 
         wrap.append(inner);
 
         return wrap;
     }
 
-    Notification.prototype.open = function(newText){
+    Notification.prototype.open = function(opts){
+        console.log('??')
+        if(opts) this.options = Object.assign({}, this.options, opts);
         var self = this;
 
-        if(newText) this.notiEl.childNodes[0].innerText = newText;
+        this.notiEl.childNodes[0].innerText = this.options.text;
+
+        // if(imageUrl){}
         this.notiEl.classList.add('on');
 
         setTimeout(function(){
@@ -130,39 +155,52 @@
         }
     };
 
+    var appendToBody = function(data){
+        myListEl.innerHTML = '';
+        listEl.innerHTML = '';
+
+        if(!data) return false;
+
+        for(var i = 0; i < data.length; i++){
+            if(data[i].checked === "true"){
+                var item = new CheckItem('true', data[i]);
+                myListEl.append(item);
+                myListArr.push(data[i]);
+    
+            }else{
+                var item = new CheckItem('false', data[i]);
+                listEl.append(item);
+                listArr.push(data[i]);
+            }
+        } 
+    };
+
+    var saveData = function(){
+        var newData = myListArr.concat(listArr);
+
+        newData.sort(function(a, b){
+            return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
+        });
+
+        localStorage.setItem(TRIP_CHECK_LIST.CHECK_LIST_DATA, JSON.stringify(newData));
+        notification.open({ 
+            text : TRIP_CHECK_LIST.NOTI_TEXT_SAVE
+        });
+    };
+
     var notification = new Notification();
     
     data = JSON.parse(data).sort(function(a, b){
         return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
     });
-
-    for(var i = 0; i < data.length; i++){
-        if(data[i].checked === "true"){
-            var item = new CheckItem('true', data[i]);
-            // myListEl.append(createItem('myList', data[i]));
-            myListEl.append(item);
-            myListArr.push(data[i]);
-
-        }else{
-            var item = new CheckItem('false', data[i]);
-            listEl.append(item);
-            listArr.push(data[i]);
-        }
-    }        
-    
+           
+    appendToBody(data);
     searchInput.focus();
 
     saveBtnEl.addEventListener('click', function(){
         if(storageAvailable('localStorage')){
-            var newData = myListArr.concat(listArr);
-
-            newData.sort(function(a, b){
-                return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
-            });
-
-            localStorage.setItem(CHECK_LIST_DATA, JSON.stringify(newData));
+            saveData();
             
-            notification.open();
         }else {
             notification.open('Error');
             // 슬픈 소식, localStorage를 사용할 수 없습니다.
@@ -171,15 +209,21 @@
 
     searchInput.addEventListener('input', function(e){
         var val = e.target.value;
-        console.log(val)
-        
-        if(val !== ''){
-            console.log('검색');
+
+        if(val === ''){
+            appendToBody(data);
+        }else{
+            var searchData = [];
+            
+            for(var i = 0; i < data.length; i++){
+                if(data[i].name.includes(val)){
+                    searchData.push(data[i]);
+                }
+            }
+            
+            appendToBody(searchData);
         }
     });
-    
-
-    
 })();
 
 // [TODO]
