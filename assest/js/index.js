058df699d2c74d70fcc9b726b3ef4961af8a16eb
leeyoonseo <okayoon.lee@gmail.com>
2020-07-26 23:14:28 +0900

    데이터 중복 저장되는 버그처리, 가이드 만드는중..중..

--- assest/js/index.js
+++ assest/js/index.js
@@ -6,18 +6,18 @@
         NOTI_TEXT_DEFAULT : '완료되었습니다.',
         NOTI_TEXT_SAVE : '저장되었습니다.',
         NOTI_TEXT_SEARCH_EMPTY : '검색어를 입력해주세요.',
-    }
+        NOTI_TEXT_ERROR : '오류입니다.'
+    };
 
-    // localStorage.removeItem(CHECK_LIST_DATA);
+    // dev
+    // localStorage.removeItem(TRIP_CHECK_LIST.CHECK_LIST_DATA);
     var data = localStorage.getItem(TRIP_CHECK_LIST.CHECK_LIST_DATA) || '[{"id":"C01","checked":"false","name":"여벌 옷"},{"id":"C02","checked":"false","name":"실내 복"},{"id":"C03","checked":"false","name":"수영복"},{"id":"C04","checked":"false","name":"모자"},{"id":"C05","checked":"false","name":"슬리퍼"},{"id":"C06","checked":"false","name":"샴푸&린스"},{"id":"C07","checked":"false","name":"바디워시"},{"id":"C08","checked":"false","name":"클렌징"},{"id":"C09","checked":"false","name":"스킨케어"},{"id":"C010","checked":"false","name":"마스크팩"},{"id":"C011","checked":"false","name":"드라이기&고데기"},{"id":"C012","checked":"false","name":"수건"},{"id":"C013","checked":"false","name":"화장품"},{"id":"C014","checked":"false","name":"여권"},{"id":"C015","checked":"false","name":"휴지&물티슈"},{"id":"C016","checked":"false","name":"카메라"},{"id":"C017","checked":"false","name":"충전기"},{"id":"C018","checked":"false","name":"노트북"},{"id":"C019","checked":"false","name":"선글라스"},{"id":"C020","checked":"false","name":"선크림"},{"id":"C021","checked":"false","name":"우산, 비옷"},{"id":"C022","checked":"false","name":"이어폰"},{"id":"C023","checked":"false","name":"비상약"},{"id":"C024","checked":"false","name":"서브가방"},{"id":"C025","checked":"false","name":"지퍼백"},{"id":"C026","checked":"false","name":"속옷"},{"id":"C027","checked":"false","name":"면도기"},{"id":"C028","checked":"false","name":"가이드북"},{"id":"C029","checked":"false","name":"돼지코"}]';
-
     var myListArr = [];
     var listArr = [];
 
     var myListEl = document.querySelector('.app__mylist .list__wrap');
     var listEl = document.querySelector('.app__list .list__wrap');
     var searchInput = document.querySelector('#searchInput');
-    // var searchButton = document.querySelector('#searchButton');
     var saveBtnEl = document.querySelector('#saveButton');
 
     // polyfill
@@ -188,22 +188,52 @@
         });
     };
 
-    var notification = new Notification();
+    var showSearchResult = function(val){
+        var searchData = [];
+
+        for(var i = 0; i < data.length; i++){
+            if(data[i].name.includes(val)){
+                searchData.push(data[i]);
+            }
+        }
+
+        myListEl.innerHTML = '';
+        listEl.innerHTML = '';
+
+        for(var i = 0; i < searchData.length; i++){
+            if(searchData[i].checked === "true"){
+                var item = new CheckItem('true', searchData[i]);
+                myListEl.append(item);
     
+            }else{
+                var item = new CheckItem('false', searchData[i]);
+                listEl.append(item);
+            }
+        } 
+    };
+
+    var notification = new Notification();
+        
     data = JSON.parse(data).sort(function(a, b){
         return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
     });
-           
+
     appendToBody(data);
     searchInput.focus();
 
+
+
+
+
+    // handler
     saveBtnEl.addEventListener('click', function(){
         if(storageAvailable('localStorage')){
             saveData();
             
         }else {
-            notification.open('Error');
-            // 슬픈 소식, localStorage를 사용할 수 없습니다.
+            notification.open({
+                text : TRIP_CHECK_LIST.NOTI_TEXT_ERROR
+            });
         }
     });
 
@@ -212,18 +242,12 @@
 
         if(val === ''){
             appendToBody(data);
+
         }else{
-            var searchData = [];
-            
-            for(var i = 0; i < data.length; i++){
-                if(data[i].name.includes(val)){
-                    searchData.push(data[i]);
-                }
-            }
-            
-            appendToBody(searchData);
+            showSearchResult(val);
         }
     });
+
 })();
 
 // [TODO]
@@ -233,3 +257,4 @@
 // true, false되는지 확인
 // 데이터 id값 생성(날짜시간초등등)
 // 리스트가 하나도 없을때 안내
+// 초성검색..?
