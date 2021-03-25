1db8036d38162e8c6616f465740798992793c0cf
leeyoonseo <okayoon.lee@gmail.com>
2021-03-25 15:04:15 +0900

    uglify, sourcemap, concat 추가

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,12 +1,17 @@
 const { src, dest } = require('gulp');
 const babel = require('gulp-babel');
 const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
 
 exports.default = function() {
     return src('src/js/*.js')
+        .pipe(sourcemaps.init())
         .pipe(babel({
             presets: ['@babel/env']
         }))
+        .pipe(concat('bundle.min.js'))
+        .pipe(uglify())
         .pipe(sourcemaps.write('.'))
         .pipe(dest('dist/'));
 }
